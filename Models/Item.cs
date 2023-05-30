6faa0b0168d30d6258c2ae388283b0ea4e1a4a85
koki sato <agricola.basic@gmail.com>
2023-05-30 05:25:35 +0000

    カテゴリー設定追加

--- Models/Item.cs
+++ Models/Item.cs
@@ -13,7 +13,7 @@ public class Item
     [Column(TypeName = "decimal(18,4)")]
     public decimal Price { get; set; }
     public long? Ticks { get; set; }
-    public IList<Category>? Categories { get; set; }
+    public ISet<Category>? Categories { get; set; }
     public IList<Event>? Events { get; set; }
 
     public DateTime? LastSwapDate => Events?.MaxBy(e => e.Date)?.Date;
