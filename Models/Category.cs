a20ba4a2d97776b11eff19bafdbedb7d84f07fd9
koki sato <agricola.basic@gmail.com>
2023-06-06 02:47:50 +0000

    カテゴリ編集実装

--- Models/Category.cs
+++ Models/Category.cs
@@ -9,4 +9,17 @@ public class Category
     public String? Name { get; set; } 
     public IList<Item>? Items { get; set;}
 
+    public override int GetHashCode()
+    {
+        return Name!.GetHashCode() ^ CategoryId!.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if(obj is null) return false;
+        Category? cat = obj as Category;
+        if(cat == null)return false;
+        return Name == cat.Name && CategoryId == cat.CategoryId;
+    }
+
 }
