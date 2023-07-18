428eb1da71130b7c35d8aa881ec86eef8f6bce15
39raKyouko <agricola.basic@gmail.com>
2023-07-19 00:22:55 +0900

    test

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("Portfolio-API.ServerAPI", client =>
-    client.BaseAddress = new Uri("http://localhost:5184"))
+    client.BaseAddress = new Uri("https://localhost:5184"))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
@@ -18,8 +18,7 @@ builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("https://ExpendablesManagementApp.onmicrosoft.com/API/API.Access");
+    options.ProviderOptions.LoginMode = "redirect";
 });
-
-
 
 await builder.Build().RunAsync();
