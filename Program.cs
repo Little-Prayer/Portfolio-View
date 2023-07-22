ffe3b69a2b3800504dba6a682e75b7679d164788
39raKyouko <agricola.basic@gmail.com>
2023-07-22 22:18:31 +0900

    認証を有効化

--- Program.cs
+++ Program.cs
@@ -8,4 +8,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5184") });
 
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    //options.ProviderOptions.DefaultAccessTokenScopes.Add("{SCOPE URI}");
+    options.ProviderOptions.LoginMode = "redirect";
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+});
+
 await builder.Build().RunAsync();
