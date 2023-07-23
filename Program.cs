ebbd2d3fc2801226f3ee3c432b10344a80ff5dbd
39raKyouko <agricola.basic@gmail.com>
2023-07-24 00:33:20 +0900

    ItemとSwap対応

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Portfolio_View;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5184") });
+builder.Services.AddHttpClient("Portfolio-API.ServerAPI", client =>
+    client.BaseAddress = new Uri("https://localhost:5184"))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("Portfolio_API.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://ExpendablesManagementApp.onmicrosoft.com/1f47fe80-c7f5-4cef-a27e-da7560f63d4f/API.Access");
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5184") });
 
 await builder.Build().RunAsync();
